dac74d514a139dccdab55c8735cb7b69bda1b0a1
tezoskorea <support@tezoskorea.com>
2019-07-07 23:50:56 +0900

    quickstart

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tezos Quick build and Sync
+
+- pre-compiled binary: tezos-south-asia
+- mainnet-quicksync: tzdutch
+- Environment: Ubuntu 16.04 LTS, 18.04 LTS (aws, google cloud)
+
+## How to Use
+```
+curl "https://gitlab.com/tezoskorea/quickstart/raw/master/quickstart.sh" | bash
+```
---
+++ quickstart.sh
@@ -0,0 +1,17 @@
+echo ============================================
+echo thanks for tezos-south-asia and tzdutch!
+echo ============================================
+
+sudo apt update -y
+sudo apt install -y wget
+wget https://gitlab.com/tezos-southeast-asia/tezosinstaller/raw/master/install.sh -O install.sh
+sudo chmod 755 install.sh
+sudo ./install.sh mainnet
+tezos-node identity generate
+sudo apt update -y
+sudo apt install -y liblz4-tool jq
+wget -c http://quicksync.tzdutch.com/mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full.tar.lz4
+lz4 -d mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full.tar.lz4 | tar xf -
+rm mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full.tar.lz4
+tezos-node snapshot import mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full
+rm mainnet-BMKkyoQRCUHh2nLxZE4o6q3ExQDeGntNsdMKTbzq6gEXRg6HguV.full
