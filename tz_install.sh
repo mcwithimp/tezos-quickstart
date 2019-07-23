3160f5bda8fa71a5f81b6c9ce571af91c50dcd95
tezoskorea <support@tezoskorea.com>
2019-07-23 17:25:30 +0900

    install script

---
+++ tz_install.sh
@@ -0,0 +1,38 @@
+usage() {
+  echo "Usage: $0 <alphanet|mainnet>"
+}
+
+case "$1" in
+  alphanet)
+    ;;
+  mainnet)
+    ;;
+  *) usage
+    exit 1
+    ;;
+esac
+
+NET=$1
+
+sudo apt update || exit 1
+sudo apt install -y rsync git make m4 build-essential patch unzip wget || exit 1
+wget http://security.ubuntu.com/ubuntu/pool/main/b/bubblewrap/bubblewrap_0.2.1-1ubuntu0.1_amd64.deb || exit 1
+sudo dpkg -i bubblewrap_0.2.1-1ubuntu0.1_amd64.deb || exit 1
+wget https://github.com/ocaml/opam/releases/download/2.0.3/opam-2.0.3-x86_64-linux  || exit 1
+sudo cp opam-2.0.3-x86_64-linux /usr/local/bin/opam || exit 1
+sudo chmod a+x /usr/local/bin/opam
+git clone https://gitlab.com/tezos/tezos.git || exit 1
+cd tezos
+git checkout $NET || exit 1
+export OPAMNO=true
+opam init --bare  || exit 1
+export OPAMNO=false
+sudo apt install -y libev-dev libgmp-dev pkg-config libhidapi-dev || exit 1
+make build-deps || exit 1
+eval $(opam env) || exit 1
+make || exit 1
+export PATH=~/tezos:$PATH
+source ./src/bin_client/bash-completion.sh
+export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y
+
+echo "Finished!"
