70c60e718ff405f493111b7effbab1ffa200db58
tezoskorea <support@tezoskorea.com>
2019-08-20 12:44:54 +0900

    modified_mac_script

--- tz_install_mac.sh
+++ tz_install_mac.sh
@@ -17,9 +17,9 @@ NET=$1
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 1
 brew update || exit 1
 brew install git gmp libev pkg-config hidapi || exit 1
-xcode-select --install || exit 1
+# xcode-select --install || exit 1
 wget https://github.com/ocaml/opam/releases/download/2.0.3/opam-2.0.3-x86_64-darwin || exit 1
-sudo mv opam-2.0.0-x86_64-darwin /usr/local/bin/opam || exit 1
+sudo mv opam-2.0.3-x86_64-darwin /usr/local/bin/opam || exit 1
 sudo chmod a+x /usr/local/bin/opam
 echo "f25a98ff5a45bd2ad7ce1b9496503c505ca8cd38525dcd11be04b9203e54cbff  /usr/local/bin/opam" | shasum -a 256 -c  
 git clone https://gitlab.com/tezos/tezos.git || exit 1
