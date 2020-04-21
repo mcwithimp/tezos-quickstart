3b37456218734c0e9673de73b3134b65626b9743
tezoskorea <support@tezoskorea.com>
2020-04-21 08:54:52 +0000

    for carthagenet

--- quickstart.sh
+++ quickstart.sh
@@ -6,7 +6,7 @@ sudo apt update -y
 sudo apt install -y wget
 wget https://gitlab.com/tezos-southeast-asia/tezosinstaller/raw/master/install.sh -O install.sh
 sudo chmod 755 install.sh
-sudo ./install.sh mainnet
+sudo ./install.sh carthagenet
 tezos-node identity generate
 sudo apt update -y
 sudo apt install -y gzip jq screen
